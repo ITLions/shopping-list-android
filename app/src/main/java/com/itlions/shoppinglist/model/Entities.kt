90536db9513238fc754694a24a51221f8ca3a016
Konstantin Kiriushyn <navigator2904@gmail.com>
2015-10-17 20:27:40 +0200

    Entites added

---
+++ app/src/main/java/com/itlions/shoppinglist/model/Entities.kt
@@ -0,0 +1,12 @@
+package com.itlions.shoppinglist.model
+
+import java.sql.Date
+
+/**
+ * TODO implement javadoc
+ */
+data class Category(var id : Int, var name : String, var description : String, var icon : String)
+data class Unit(var id : Int, var name : String)
+data class Product(var id : Int, var name : String, var description : String, var icon : String, var category: Category, var unit: Unit)
+data class ListItem(var id: Int, var checked : Boolean, var product: Product, var productList : ProductList, var amount : Int)
+data class ProductList(var id: Int, var name : String, var createdDate : Date)

ceb14503bf3d448bce49889a6b6a175b80acb86c
Konstantin Kiriushyn <navigator2904@gmail.com>
2015-10-18 15:39:29 +0200

    Added extension to copy database from assets to app internal folder

--- app/src/main/java/com/itlions/shoppinglist/model/Extensions.kt
+++ app/src/main/java/com/itlions/shoppinglist/model/Extensions.kt
@@ -1,18 +1,29 @@
 package com.itlions.shoppinglist.model
 
 import android.app.Activity
+import android.content.Context
 import org.jetbrains.anko.async
 import org.jetbrains.anko.uiThread
+import java.io.FileOutputStream
 
 /**
  * TODO implement javadoc
  */
-fun Activity.asyncWithResult<T>(task : () -> T?, after : (T?) -> Any) {
-    var result : T?
+fun Activity.asyncWithResult<T>(task: () -> T, after: (T) -> Any) {
+    var result: T
     async {
         result = task()
         uiThread {
             after(result)
         }
     }
+}
+
+fun Context.copyDatabaseFromAssets(fromFile: String, toFile: String) {
+    var inSt = assets.open(fromFile)
+    var outFile = getDatabasePath(toFile)
+    var out = FileOutputStream(outFile)
+    inSt.copyTo(out)
+    inSt.close()
+    out.close()
 }
